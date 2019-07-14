330d0b0676fabc51d262bac3323ef69fcc536283
Sam Marsh <smarsh09@gmail.com>
2019-07-14 01:12:32 -0500

    Small changes to Stats/Skills.

--- src/app/model/hero.ts
+++ src/app/model/hero.ts
@@ -2,6 +2,7 @@ import { Origin } from 'src/app/data/origin';
 import { Data } from 'src/app/data/data';
 import { Archetype } from 'src/app/data/archetype';
 import { TraversalPower } from 'src/app/data/traversal';
+import { Stats } from '../data/stat';
 
 export class Hero {
   heroName?: string;
@@ -17,7 +18,7 @@ export class Hero {
   archetype?: Archetype;
   traversal?: TraversalPower;
 
-  stats: { [stat: string]: number } = { ["Might"]: 12 };
+  stats: { [stat: string]: number } = {};
   skills: { [skill: string]: boolean } = {};
   getMod(stat: string, skill?: string): number {
     let mod = Math.round((this.stats[stat] - 11) / 2);
@@ -25,5 +26,9 @@ export class Hero {
     if (skill && this.skills[skill]) { mod += this.proficiency(); }
 
     return mod;
+  }
+
+  constructor() {
+    Stats.forEach(s => this.stats[s.name] = 8);
   }
 }
