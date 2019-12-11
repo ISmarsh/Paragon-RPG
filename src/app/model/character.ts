e9466ba56eb71067062eb3512ad1f1298c8634d0
Sam Marsh <smarsh09@gmail.com>
2019-12-10 19:17:24 -0600

    Some visual changes/fixes/cleanup.

--- src/app/model/character.ts
+++ src/app/model/character.ts
@@ -14,6 +14,7 @@ import { MainPower, MainPowers } from '../data/main-power';
 import { AncillaryPower, AncillaryPowers } from '../data/ancillary-power';
 import { Progression, Level } from '../data/leveling';
 import { Features } from '../core/enums';
+import { Repository } from './repository';
 
 @jsonObject
 @Reflect.metadata("name", "Character")
@@ -59,6 +60,8 @@ export class Character extends Entity {
     else if (to && to.length) {
       this.originSkills.push(to);
     }
+
+    Repository.save(Character, this);
   }
   public getOriginSkills(): string[] {
     const array = this.originSkills.concat(new Array<string>(this.origin.proficiencyCount));
